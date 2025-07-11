7af1aa2c1a8b69522d3e27a2e5bbe38f066e313b
Gayani_Jayasundara <gayani24@stu.codepark.university>
2025-07-11 22:31:19 +0530

    Add Admin page

--- Client/src/Components/AdminPanel/AdminPanel.js
+++ Client/src/Components/AdminPanel/AdminPanel.js
@@ -2,12 +2,16 @@ import React from 'react'
 import AdminNavBar from "./AdminNavBar";
 import AdminFooter from "./AdminFooter";
 import AdminScreen from './AdminScreen';
+import DashboardCard from './Dashboard/DashboardCard';
+import DashboardChart from './Dashboard/DashboardChart';
 
 const AdminPanel = () => {
   return (
     <div>
       <AdminNavBar/>
       <AdminScreen/>
+      <DashboardCard/>
+      <DashboardChart/>
       <AdminFooter/>
     </div>
   )
--- Client/src/Components/AdminPanel/AdminScreen.js
+++ Client/src/Components/AdminPanel/AdminScreen.js
@@ -4,6 +4,8 @@ import AdoptingRequests from './AdoptingRequests'
 import AdoptedHistory from './AdoptedHistory'
 import ApprovedRequests from './ApprovedRequests'
 import AdminCreateArticle from './AdminCreateArticle'
+import AdminDashboard from './Dashboard/AdminDashboard'
+import AdminAppointments from './Appointment'
 
 const AdminScreen = () => {
   const [screen, setScreen] = useState('postingPet')
@@ -12,6 +14,8 @@ const AdminScreen = () => {
     <div className='admin-screen-container'>
       <div className='admin-screen-left'>
         <div>
+          <p onClick={() => setScreen('dashboard')}>Dashboard</p>
+          <p onClick={() => setScreen('appointment')}>Appointments</p>
           <p onClick={() => setScreen('postingPet')}>Post Pet Requests</p>
           <p onClick={() => setScreen('approvedRequests')}>Approved Pets</p>
           <p onClick={() => setScreen('adoptingPet')}>Adoption Requests</p>
@@ -21,6 +25,8 @@ const AdminScreen = () => {
         </div>
       </div>
       <div className='admin-screen-right'>
+        {screen === 'dashboard' && <AdminDashboard />}
+        {screen === 'appointment' && <AdminAppointments />}
         {screen === 'postingPet' && <PostingPets />}
         {screen === 'approvedRequests' && <ApprovedRequests />}
         {screen === 'adoptingPet' && <AdoptingRequests />}
