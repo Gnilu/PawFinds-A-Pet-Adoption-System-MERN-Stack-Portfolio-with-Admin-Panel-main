ad4bbdf7a5f67ce1b2cccb13c5dcd2b62d5774dd
Gayani_Jayasundara <gayani24@stu.codepark.university>
2025-07-13 23:09:42 +0530

    responsivenessimprove

--- Client/src/Components/Services/Services.js
+++ Client/src/Components/Services/Services.js
@@ -26,6 +26,21 @@ const Services = () => {
         .post-pet {
           max-width: 50vw;
 }
+
+@media (max-width: 600px) {
+.main-container {
+          display: flex;
+          flex-direction: column;
+        }
+
+        .adopt-pet {
+          max-width: 100%;
+}
+
+.post-pet {
+          max-width: 100%;
+}
+}
         `}</style>
     </div>
   )
