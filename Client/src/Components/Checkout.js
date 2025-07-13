8033019ec3d675ceedace86d7253fe44ba62127f
JayaniKodithuwakku <jayani24@stu.codepark.university>
2025-07-14 02:28:34 +0530

    fix path error

--- Client/src/Components/Checkout.js
+++ Client/src/Components/Checkout.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import './Checkout.css';
-import { useToast } from '../ToastContext';
+import { useToast } from './ToastContext';
 
 
 const Checkout = () => {
